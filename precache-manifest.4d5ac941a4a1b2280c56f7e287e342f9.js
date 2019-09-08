77cf4a4fa9935b29119b20c5b3a2a427edeb3919
Andrew <Andrew01.01.1997@gmail.com>
2019-09-08 21:55:14 +0300

    Updates

---
+++ precache-manifest.4d5ac941a4a1b2280c56f7e287e342f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c596a1834c243df5f5be60240a4c0939",
+    "url": "/pokedex-last/index.html"
+  },
+  {
+    "revision": "d8f67609624dc01c8a02",
+    "url": "/pokedex-last/static/css/2.848db5fd.chunk.css"
+  },
+  {
+    "revision": "3098ce888e440559a0cc",
+    "url": "/pokedex-last/static/css/main.b76d9f9c.chunk.css"
+  },
+  {
+    "revision": "d8f67609624dc01c8a02",
+    "url": "/pokedex-last/static/js/2.5f6c162e.chunk.js"
+  },
+  {
+    "revision": "3098ce888e440559a0cc",
+    "url": "/pokedex-last/static/js/main.78305f23.chunk.js"
+  },
+  {
+    "revision": "297652b32855a54fea4d",
+    "url": "/pokedex-last/static/js/runtime~main.72b88be9.js"
+  }
+]);
