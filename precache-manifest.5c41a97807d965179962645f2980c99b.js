11e307fd5d1983a84470486a8608ccc7f1453878
Andrew <Andrew01.01.1997@gmail.com>
2019-09-10 12:08:38 +0300

    Updates

--- precache-manifest.5c41a97807d965179962645f2980c99b.js
+++ precache-manifest.5c41a97807d965179962645f2980c99b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "102f320a4c9ca468677fb7d579cf9c8b",
+    "revision": "a3e7e8e07a651f1d7a9faa3c275e08c2",
     "url": "/pokedex-last/index.html"
   },
   {
-    "revision": "e56966abe8560a47dd4d",
+    "revision": "abbe38e30cdd2ddcc783",
     "url": "/pokedex-last/static/css/2.848db5fd.chunk.css"
   },
   {
-    "revision": "c0b8980b966ddd8b8e25",
+    "revision": "babe59561a53363d60c3",
     "url": "/pokedex-last/static/css/main.310f57a2.chunk.css"
   },
   {
-    "revision": "e56966abe8560a47dd4d",
-    "url": "/pokedex-last/static/js/2.a9024f0f.chunk.js"
+    "revision": "abbe38e30cdd2ddcc783",
+    "url": "/pokedex-last/static/js/2.1721d610.chunk.js"
   },
   {
-    "revision": "c0b8980b966ddd8b8e25",
-    "url": "/pokedex-last/static/js/main.3c1a838f.chunk.js"
+    "revision": "babe59561a53363d60c3",
+    "url": "/pokedex-last/static/js/main.5cb91150.chunk.js"
   },
   {
     "revision": "297652b32855a54fea4d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokedex-last/precache-manifest.c593e86c3af49273257e7f46716b7534.js"
+  "/pokedex-last/precache-manifest.5c41a97807d965179962645f2980c99b.js"
 );
 
 self.addEventListener('message', (event) => {
